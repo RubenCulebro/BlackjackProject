edd1650e70b689322dc1d83617a95420fcfa1011
Ruben Culebro <rubenculebro@Rubens-MacBook-Air.local>
2023-04-04 23:41:45 -0230

    Add a function used to create a deck of cards.

--- Blackjack.py
+++ Blackjack.py
@@ -1,14 +1,22 @@
-# This is a sample Python script.
+def cardDeck():
+    deck = []
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    points = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    for suit in range(len(suits)):
+        for rank in range(len(ranks)):
+            card = [ranks[rank], suits[suit], points[rank]]  # reference: suits [0] #ranks[1]
+            deck.append(card)
+    return deck
 
 
 def main():
-    print("Blackjack Project")
+    print("Blackjack Project\n")
+    deck = cardDeck()
+    print(deck)
 
 
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
