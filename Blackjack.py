1e457d063545953cd3f733ca9189b7e4d34e85c9
Ruben Culebro <rubenculebro@Rubens-MacBook-Air.local>
2023-04-10 14:14:22 -0230

    Add BlackjackGame, dealCard, aceCard and Header functions

--- Blackjack.py
+++ Blackjack.py
@@ -1,4 +1,11 @@
 import random
+
+
+def showHeader():
+    print("BLACKJACK!")
+    print("Blackjack payout is 3:2")
+
+
 def cardDeck():
     deck = []
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -11,16 +18,58 @@ def cardDeck():
             deck.append(card)
     return deck
 
+
+def startBlackjackGame(deck):
+    dealerScore = 0
+    dealerHand = []
+    playerScore = 0
+    playerHand = []
+
+    for i in range(1, 5):
+        if i % 2 != 0:
+            turn = "player"
+            playerScore = dealCard(deck, playerHand, playerScore, turn)
+        else:
+            turn = "dealer"
+            dealerScore = dealCard(deck, dealerHand, dealerScore, turn)
+            if i == 2:
+                print()
+                print("DEALER'S SHOW CARD")
+                print(f"{dealerHand[0][0]} of {dealerHand[0][1]}")
+
+
+def dealCard(deck, hand, score, turn):
+    selectedCard = drawCard(deck)
+    aceCard(selectedCard, score, turn)
+    score += selectedCard[2]
+    hand.append(selectedCard)
+    return score
+
+
+def aceCard(selectedCard,score, turn):
+    if selectedCard[0] == "Ace" and (score < 11):
+        if turn == "player":
+            aceValue = int(input("\nAce value equals 1 or 11?:"))
+            if aceValue == 1:
+                selectedCard[2] = 1
+            elif aceValue == 11:
+                selectedCard[2] = 11
+        elif turn == "dealer":
+            selectedCard[2] = 11 # If dealer's score is < 10 assign the value of 11
+    elif selectedCard[0] == "Ace" and (score >= 11):
+        selectedCard[2] = 1
+
+
 def drawCard(deck):
     selectCard = random.choice(deck)
     deck.remove(selectCard)
-
     return selectCard
 
+
 def main():
-    print("Blackjack Project\n")
     deck = cardDeck()
-    print(deck)
+    showHeader()
+    startBlackjackGame(deck)
 
 
 if __name__ == '__main__':
